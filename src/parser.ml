2b614b0f60b23a5996c33ed4fa9c70cde3e651f5
eliottdhommee <eliottdhommee@gmail.com>
2022-05-30 17:31:39 +0200

    added grammar for one character and started parser

--- src/parser.ml
+++ src/parser.ml
@@ -1,3 +1,12 @@
+let read_lines file process =
+  let in_ch = open_in file in
+  let rec read_line () =
+    let line = try input_line in_ch with End_of_file -> "" in
+    process line;
+    read_line ()
+    in
+  read_line ()
+
 let parse_grammar grammar_file =
   let combos = [] in
   combos
--- taskell.md
+++ taskell.md
@@ -1,6 +1,8 @@
 ## To Do
 
 - Create grammar file
+    * [ ] get combo list for each character
+    * [x] get combo List for Scorpion
 - Parse grammar file
 - Convert grammar into transition
 - Exectute transition from user input
