1323711cff24ece96b09ff2ac815902832e91425
mgalliou <matthias.galliou@gmail.com>
2022-05-31 13:39:13 +0200

    change move implementation

--- src/move.ml
+++ src/move.ml
@@ -1,73 +1,11 @@
-type t = 
-  | Left 
-  | Right 
-  | Up 
-  | Down 
-  | Front_Punch 
-  | Back_Punch 
-  | Front_Kick 
-  | Back_Kick
-  | Throw
-  | Tag
-  | Flip_Stance
-  | Unknown
+type t = {
+  name : string;
+  keycode : Sdlkeycode.t
+}
 
-let to_string m =
-  match m with
-  | Left -> "Left"
-  | Right -> "Right"
-  | Up -> "Up"
-  | Down -> "Down"
-  | Front_Punch -> "[FP]"
-  | Back_Punch -> "[BP]"
-  | Front_Kick -> "[FK]"
-  | Back_Kick -> "[BK]"
-  | Throw -> "Throw"
-  | Tag -> "Tag"
-  | Flip_Stance -> "Flip Stance"
-  | Unknown -> "Unknown"
-  
-let from_string s =
-  match s with
-  | "Left" -> Left
-  | "Right" -> Right
-  | "Up" -> Up
-  | "Down" -> Down
-  | "[FP]" -> Front_Punch
-  | "[BP]" -> Back_Punch
-  | "[FK]" -> Front_Kick
-  | "[BK]" -> Back_Kick
-  | "Throw" -> Throw
-  | "Tag" -> Tag
-  | "Flip Stance" -> Flip_Stance
-  | _ -> Unknown
+let _new name key_str =
+{
+  name = name;
+  keycode = Sdlkeycode.of_string key_str
+}
 
-let to_keycode m =
-  match m with
-  | Left -> Sdlkeycode.Left
-  | Right -> Sdlkeycode.Right
-  | Up -> Sdlkeycode.Up
-  | Down -> Sdlkeycode.Down
-  | Front_Punch -> Sdlkeycode.X
-  | Back_Punch -> Sdlkeycode.D
-  | Front_Kick -> Sdlkeycode.Z
-  | Back_Kick -> Sdlkeycode.S
-  | Throw -> Sdlkeycode.A
-  | Tag -> Sdlkeycode.E
-  | Flip_Stance -> Sdlkeycode.W
-  | Unknown -> Sdlkeycode.Unknown
-
-let from_keycode kc =
-  match kc with
-  | Sdlkeycode.Left -> Left
-  | Sdlkeycode.Right -> Right
-  | Sdlkeycode.Up -> Up
-  | Sdlkeycode.Down -> Down
-  | Sdlkeycode.X -> Front_Punch
-  | Sdlkeycode.D -> Back_Punch
-  | Sdlkeycode.Z -> Front_Kick
-  | Sdlkeycode.S -> Back_Kick
-  | Sdlkeycode.A -> Throw
-  | Sdlkeycode.E -> Tag
-  | Sdlkeycode.W -> Flip_Stance
-  | _ -> Unknown
--- src/move.mli
+++ src/move.mli
@@ -1,21 +1,6 @@
-type t = 
-  | Left 
-  | Right 
-  | Up 
-  | Down 
-  | Front_Punch 
-  | Back_Punch 
-  | Front_Kick 
-  | Back_Kick
-  | Throw
-  | Tag
-  | Flip_Stance
-  | Unknown
+type t = {
+  name : string;
+  keycode : Sdlkeycode.t
+}
 
-val to_string : t -> string
-
-val from_string : string -> t
-
-val to_keycode : t -> Sdlkeycode.t
-
-val from_keycode : Sdlkeycode.t -> t
+val _new : string -> string -> t
