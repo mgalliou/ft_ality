13066b85ccf516a2452f724a3444ba05cb8fc2b5
mgalliou <matthias.galliou@gmail.com>
2022-06-03 12:44:47 +0200

    refactoring of Game module

--- src/game.ml
+++ src/game.ml
@@ -1,51 +1,34 @@
-let process_keydown_event (e : Sdlevent.keyboard_event) =
-  (*
-  match e.keycode |> Move.from_keycode with
-  | Move.Unknown -> ()
-  | _ -> print_endline ((Sdlkeycode.to_string e.keycode) ^ " " ^ (e.ke_state |> Sdlevent.string_of_state))
-*)
- ()
+let new_state_with_input_str input (current_state : State.t) (machine : Machine.t) =
+  match List.find (fun (s, t) -> String.equal input s) current_state.transitions with
+  | ("", _) -> machine.states
+  | (_, state) when state.input_line = [] -> machine.states
+  | (input_str, to_state) -> to_state
 
-let process_keyup_event (e : Sdlevent.keyboard_event) =
-  (*
-  match e.keycode |> Move.from_keycode with
-  | Move.Unknown -> ()
-  | _ -> print_endline ((Sdlkeycode.to_string e.keycode) ^ " " ^ (e.ke_state |> Sdlevent.string_of_state))
-*)
- ()
-
-let find_move_str (e: Sdlevent.keyboard_event) bindings =
+let input_str_from_key_event (e : Sdlevent.keyboard_event) bindings =
   match List.find_opt (fun (b : Move.t) -> b.keycode = e.keycode) bindings with
   | Some b -> b.name
   | None -> ""
 
-let match_event e bindings =
+let new_state_with_event e state (machine : Machine.t) =
   match e with
-  | Sdlevent.Quit _ -> Sdl.quit (); exit 0;
-  | Sdlevent.KeyUp e -> find_move_str e bindings
-  | _ -> ""
+  | Sdlevent.Quit _ -> Sdl.quit (); exit 0
+  | Sdlevent.KeyUp e -> 
+    let input_str = input_str_from_key_event e machine.bindings in
+    let new_state = new_state_with_input_str input_str state machine in
+    let _ =  List.iter print_endline new_state.input_line in
+    let _ = State.print_combos new_state in
+    new_state
+  | _ -> state
 
-let get_next_state input_str states_root (state: State.t) =
-  match input_str with
-  | "" -> state
-  | _ ->
-    match List.find (fun (s, t) -> String.equal input_str s) state.transitions with
-    | ("", _) -> states_root
-    | (_, idle) when idle.input_line = [] -> states_root
-    | (input_str, to_state) -> to_state
+let rec event_loop current_state machine =
+  match Sdlevent.poll_event () with
+  | None -> event_loop current_state machine
+  | Some e -> 
+    let new_state = new_state_with_event e current_state machine in
+    event_loop new_state machine
 
 let run (machine: Machine.t) =
-  let rec read_loop state =
-    match Sdlevent.poll_event () with
-    | None -> (); read_loop state
-    | Some e ->
-      let input_str = match_event e machine.bindings in
-      let new_state = get_next_state input_str machine.states state in
-      List.iter print_endline state.input_line;
-      State.print_combos new_state;
-      read_loop new_state
-  in
   Sdl.init_subsystem [`VIDEO];
   let width, height = (0, 0) in
-  let window, renderer = Sdl.Render.create_window_and_renderer ~width ~height ~flags:[] in
-  read_loop machine.states
+  let _, _ = Sdl.Render.create_window_and_renderer ~width ~height ~flags:[] in
+  event_loop machine.states machine
